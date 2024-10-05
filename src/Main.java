f7f12afd2fdde32937bd7a0a88e39f8203639f20
manpreetsingh04m <ms2404mk@gmail.com>
2024-10-05 11:27:03 +0530

    added static,new,deleted concepts used

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,33 @@ import core.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Creating a finance manager that will keep a record of all transactions
+        // Creating a finance manager to track transactions
         FinanceManager financeManager = new FinanceManager();
         
-        // Creating a demo expense (e.g., Rent)
-        Transaction rentExpense = new Expense("Rent", "Monthly house rent", 1200.00, "2024-09-01");
-        financeManager.addTransaction(rentExpense);
-        
-        // Creating a demo income (e.g., Salary)
-        Transaction salaryIncome = new Income("Salary", "Monthly salary", 3000.00, "2024-09-05");
-        financeManager.addTransaction(salaryIncome);
-        
-        // Display all transactions stored inside the finance manager
+        // Adding some transactions (Expense, Income, Investment)
+        financeManager.addTransaction(new Expense("Rent", "Monthly house rent", 1200.00, "2024-09-01"));
+        financeManager.addTransaction(new Income("Salary", "Monthly salary", 3000.00, "2024-09-05"));
+        financeManager.addTransaction(new Investment("Stocks", "Investment in stocks", 5000.00, "2024-09-10"));
+
+        // Displaying the transactions
         financeManager.displayTransactionDetails();
 
-        // Initializing a transaction array to demonstrate array of objects concept
-        Transaction[] transactions = new Transaction[3];
+        // Displaying static member info for Investment class
+        System.out.println("Total Investments Created: " + Investment.getTotalCreated());
+        System.out.println("Total Investments Deleted: " + Investment.getTotalDeleted());
 
-        // Creating sample objects to add to the array
-        transactions[0] = new Expense("Groceries", "Buying vegetables and fruits", 100.00, "2024-09-08");
-        transactions[1] = new Income("Freelance", "Payment for freelance work", 500.00, "2024-09-07");
-        transactions[2] = new Expense("Electricity Bill", "Monthly utility payment", 150.00, "2024-09-10");
+        // Forcing garbage collection (to trigger finalize() and simulate deletion)
+        System.gc();
 
-        // Displaying transaction details from the array
-        for (Transaction transaction : transactions) {
-            transaction.displayTransactionDetails();
-            System.out.println("-----------------------");
+        // Give time for garbage collection to happen
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        // Displaying total after GC
+        System.out.println("After GC:");
+        System.out.println("Total Investments Deleted: " + Investment.getTotalDeleted());
     }
 }
